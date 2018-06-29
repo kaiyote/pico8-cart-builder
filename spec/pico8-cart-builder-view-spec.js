6eb19a775b63a16734fdcb536e62d09af662dfc1
Laerin <39832634+XNargaHuntress@users.noreply.github.com>
2018-06-29 15:57:50 -0400

    Initial Commit

---
+++ spec/pico8-cart-builder-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import Pico8CartBuilderView from '../lib/pico8-cart-builder-view';
+
+describe('Pico8CartBuilderView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
